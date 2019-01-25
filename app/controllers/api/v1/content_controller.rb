c68e4115251ba5d17ffb162146b0f74e292dee1e
hmalik88 <hbmalik88@gmail.com>
2019-01-25 10:46:14 -0500

    fixed expiration link and got basic working mvp

--- app/controllers/api/v1/content_controller.rb
+++ app/controllers/api/v1/content_controller.rb
@@ -1,21 +1,34 @@
 class Api::V1::ContentController < ApplicationController
+skip_before_action :authorized, only: [:create, :index, :show, :update]
 
   def create
     @content = Content.create(content_params)
     @content.clip.attach(content_params[:clip])
-    @url = @content.clip.service_url
+    @url = url_for(@content.clip)
     @content.update(url: @url)
     render json: {name: @content.name, url: @url}
   end
 
-  def delete
-    @content = Content.find_by(id: content_params[:id])
+  def destroy
+    @content = Content.find_by(id: params[:id])
+    @content.clip.purge
     @content.destroy
+    render json: {content: @content}
+  end
+
+  def update
+    @content = Content.find_by(id: params[:id])
+    @content.update(name: content_params[:name], channel: content_params[:channel])
     render json: {content: @content}
   end
 
   def index
     render json: Content.includes(:users), include: ['users']
+  end
+
+  def show
+    @content = Content.find_by(id: params[:id])
+    render json: {content: @content}, status: :accepted
   end
 
   private
