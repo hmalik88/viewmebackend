7452754c7d1854730af4ec5c11e6453048c06615
hmalik88 <hbmalik88@gmail.com>
2019-02-01 18:14:25 -0500

    built out create method in Favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,9 +2,18 @@ class FavoritesController < ApplicationController
 skip_before_action :authorized, only: [:create, :destroy]
 
   def create
+    @content = Content.find_by(id: params[])
+    @url = url_for(@content.clip)
+    @favorite = Favorite.create(content_id: favorites_params[:content_id], user_id: favorites_params[:user_id], name: favorites_params[:name], url: @url)
   end
 
   def destroy
+  end
+
+  private
+
+  def favorites_params
+    params.require(:favorite).permit(:content_id, :user_id, :name)
   end
 
 end
