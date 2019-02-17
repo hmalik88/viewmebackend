0ccb69f1328ec141487b1ec66636a66ae474e63b
hmalik88 <hbmalik88@gmail.com>
2019-02-17 07:11:15 -0500

    fixed controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,8 +1,8 @@
-class FavoritesController < ApplicationController
+class Api::V1::FavoritesController < ApplicationController
 skip_before_action :authorized, only: [:create, :destroy]
 
   def create
-    @content = Content.find_by(id: params[:content_id])
+    @content = Content.find_by(id: favorites_params[:content_id])
     @url = url_for(@content.clip)
     @favorite = Favorite.create(content_id: favorites_params[:content_id], user_id: favorites_params[:user_id], name: favorites_params[:name], url: @url)
     if @favorite.valid?
@@ -13,13 +13,15 @@ skip_before_action :authorized, only: [:create, :destroy]
   end
 
   def destroy
-
+    @favorite = Favorite.find_by(id: favorites_params[:favorite_id])
+    @favorite.destroy
+    render json: {favorite: @favorite}
   end
 
   private
 
   def favorites_params
-    params.require(:favorite).permit(:content_id, :user_id, :name)
+    params.require(:favorite).permit(:content_id, :user_id, :name, :favorite_id)
   end
 
 end
